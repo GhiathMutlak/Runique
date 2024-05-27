15298c26df83e9fad534271af223961f304c4846
Giyas MITLAK <giyas@mantasolutions.online>
2024-05-27 21:08:42 +0300

    Add dependencies

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -37,6 +37,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    implementation(libs.org.mongodb.bson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
