e75bd2be746b51eb8a6a13295e6c3ea1e4779e3b
Giyas MITLAK <ghiath.mutlak@gmail.com>
2025-02-15 16:12:01 +0100

    7.7 Implement running tracker

---
+++ run/domain/src/main/java/com/perfecta/run/domain/LocationDataCalculator.kt
@@ -0,0 +1,17 @@
+package com.perfecta.run.domain
+
+import com.perfecta.core.domain.util.location.LocationTimestamp
+import kotlin.math.roundToInt
+
+object LocationDataCalculator {
+    fun getTotalDistanceMeters(locations: List<List<LocationTimestamp>>): Int {
+        return locations
+            .sumOf { timestampsPerLine ->
+                timestampsPerLine.zipWithNext { location1, location2 ->
+                    location1.location.location.distanceTo(location2.location.location)
+                }
+                    .sum()
+                    .roundToInt()
+            }
+    }
+}
