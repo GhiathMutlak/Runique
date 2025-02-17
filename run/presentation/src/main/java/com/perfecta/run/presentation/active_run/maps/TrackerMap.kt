ab0b12accf6080f77b4ca4ec7b48cabcba0f14b7
Giyas MITLAK <ghiath.mutlak@gmail.com>
2025-02-17 23:32:16 +0100

    7.11 Draw polyline on map

--- run/presentation/src/main/java/com/perfecta/run/presentation/active_run/maps/TrackerMap.kt
+++ run/presentation/src/main/java/com/perfecta/run/presentation/active_run/maps/TrackerMap.kt
@@ -90,6 +90,8 @@ fun TrackerMap(
             zoomControlsEnabled = false
         )
     ) {
+        RuniquePolylines(locations = locations)
+
         if (!isRunFinished && currentLocation != null) {
             MarkerComposable(
                 currentLocation,
