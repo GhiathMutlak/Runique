6dbeeef15674b7bbe11d164848ea9fa1cf8013ce
Giyas MITLAK <giyas@mantasolutions.online>
2025-08-22 09:32:52 +0200

    9.3 Implement Run API

---
+++ run/network/src/main/java/com/perfecta/run/network/KtorRemoteRunDataSource.kt
@@ -0,0 +1,66 @@
+package com.perfecta.run.network
+
+import com.perfecta.core.data.networking.constructRoute
+import com.perfecta.core.data.networking.delete
+import com.perfecta.core.data.networking.get
+import com.perfecta.core.data.networking.safeCall
+import com.perfecta.core.domain.run.RemoteRunDataSource
+import com.perfecta.core.domain.run.Run
+import com.perfecta.core.domain.util.DataError
+import com.perfecta.core.domain.util.EmptyResult
+import com.perfecta.core.domain.util.map
+import io.ktor.client.HttpClient
+import io.ktor.client.request.forms.formData
+import io.ktor.client.request.forms.submitFormWithBinaryData
+import io.ktor.http.Headers
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import kotlinx.serialization.json.Json
+import com.perfecta.core.domain.util.Result
+
+
+class KtorRemoteRunDataSource(
+    private val httpClient: HttpClient
+): RemoteRunDataSource {
+
+    override suspend fun getRuns(): Result<List<Run>, DataError.Network> {
+        return httpClient.get<List<RunDto>>(
+            route = "/runs",
+        ).map { runDtos ->
+            runDtos.map { it.toRun() }
+        }
+    }
+
+    override suspend fun postRun(run: Run, mapPicture: ByteArray): Result<Run, DataError.Network> {
+        val createRunRequestJson = Json.encodeToString(run.toCreateRunRequest())
+        val result = safeCall<RunDto> {
+            httpClient.submitFormWithBinaryData(
+                url = constructRoute("/run"),
+                formData = formData {
+                    append("MAP_PICTURE", mapPicture, Headers.build {
+                        append(HttpHeaders.ContentType, "image/jpeg")
+                        append(HttpHeaders.ContentDisposition, "filename=mappicture.jpg")
+                    })
+                    append("RUN_DATA", createRunRequestJson, Headers.build {
+                        append(HttpHeaders.ContentType, "text/plain")
+                        append(HttpHeaders.ContentDisposition, "form-data; name=\"RUN_DATA\"")
+                    })
+                }
+            ) {
+                method = HttpMethod.Post
+            }
+        }
+        return result.map {
+            it.toRun()
+        }
+    }
+
+    override suspend fun deleteRun(id: String): EmptyResult<DataError.Network> {
+        return httpClient.delete(
+            route = "/run",
+            queryParameters = mapOf(
+                "id" to id
+            )
+        )
+    }
+}
